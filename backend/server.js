cc259430457c38ef65340921b14e2d8038ae5f52
Zaidkhan88 <zaidk8517@gmail.com>
2025-04-24 10:40:31 +0530

    Server correction

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,24 @@ import userRoutes from './routes/userRoutes.js';
 import boardRoutes from './routes/boardRoutes.js';
 import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 import setupMailer from './utils/mailer.js';
-
-const port = process.env.PORT || 5000;
-
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+const port = process.env.PORT ;
+const SECRET = process.env.SECRET_KEY; 
+const options = { 
+    expiresIn: '120m', 
+    algorithm: 'HS256' 
+  };
 connectDB();
 setupMailer();
 const app = express();
+app.use(cors({
+    // origin: 'http://localhost:3001',
+    origin: "https://collab-x-fe.vercel.app",
 
+
+    credentials: true, // if you're using cookies or auth headers
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -34,7 +45,21 @@ if (process.env.NODE_ENV === 'production') {
         res.send('API is running....');
     });
 }
+app.get('/good-morning', (req, res) => {
+    res.send('I hate Mondays!!');
+  });
+  app.get('/get-token', (req, res) => {
 
+    const payload = {
+      apikey: process.env.API_KEY,
+      permissions: ['allow_join','allow_mod','allow_rec','allow_upload','allow_chat','allow_hand','allow_rec_play']
+    };
+  
+    const token = jwt.sign(payload, SECRET, options);
+    res.json({ token });
+    // console.log(token);
+  });
+  
 app.use(notFound);
 app.use(errorHandler);
 
